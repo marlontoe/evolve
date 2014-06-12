b16eb9fb3b5f00a4f8d252c23488bb0cc6878a01
marlontoe <mwoe.ubuntu@gmail.com>
2014-06-12 13:41:05 +0200

    merged deedwars upstream patches from evolve-1.17 and bumped nozomi version

--- include/linux/msm_kgsl.h
+++ include/linux/msm_kgsl.h
@@ -118,7 +118,12 @@ enum kgsl_ctx_reset_stat {
 	KGSL_CTX_STAT_UNKNOWN_CONTEXT_RESET_EXT		= 0x00000003
 };
 
+#ifdef CONFIG_NOZOMI_GPU_SCALING
+#define KGSL_MAX_PWRLEVELS 10
+#else 
 #define KGSL_MAX_PWRLEVELS 5
+#endif
+
 #define KGSL_CONVERT_TO_MBPS(val) \
 	(val*1000*1000U)
 
