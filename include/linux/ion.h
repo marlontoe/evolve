01296792c8b2a1044c837267557eba823cf458a7
Warren <deed821@gmail.com>
2014-06-16 20:24:11 +1000

    fix for media
    Change-Id: I8b23240746950711ff2adb4423280aaf2a7c469b

--- include/linux/ion.h
+++ include/linux/ion.h
@@ -50,7 +50,7 @@ enum ion_heap_type {
 #define ION_HEAP_SYSTEM_CONTIG_MASK	(1 << ION_HEAP_TYPE_SYSTEM_CONTIG)
 #define ION_HEAP_CARVEOUT_MASK		(1 << ION_HEAP_TYPE_CARVEOUT)
 #define ION_HEAP_CP_MASK		(1 << ION_HEAP_TYPE_CP)
-
+#define ION_FLAG_CACHED 1
 
 /**
  * These are the only ids that should be used for Ion heap ids.
